d0c1000e32738315d4cd50662c8009bdbc13e137
edsen <edy.setiawan213@gmail.com>
2023-03-20 18:40:55 +0800

    Add async rewrites and headers functions to next.config.js for handling API requests with appropriate headers and access controls

--- next.config.js
+++ next.config.js
@@ -3,7 +3,31 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/generate",
+        destination: "https://gptexplain.vercel.app/api/generate",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/generate",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000, https://gptexplain.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
