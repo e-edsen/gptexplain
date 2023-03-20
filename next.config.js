c43d5b69044246e2d562cfba40182ce1fb540c4b
edsen <edy.setiawan213@gmail.com>
2023-03-20 18:26:24 +0800

    Remove unnecessary code related to CORS configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,33 +5,5 @@ const nextConfig = {
   },
 }
 
-const cors = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/generate",
-        destination: "https://gptexplain.vercel.app/api/generate",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/generate",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000, https://gptexplain.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "POST",
-          },
-        ],
-      },
-    ];
-  },
-};
 
-
-module.exports = nextConfig, cors;
+module.exports = nextConfig
